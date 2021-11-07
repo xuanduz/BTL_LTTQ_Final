e0a554ffef86c642b3566f846c92306605bf4aa8
xuanduz <xuanduc942001@gmail.com>
2021-11-07 08:48:44 +0700

    test

---
+++ BTL_DT/BTL_DT/DataBaseProcess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace BTL_DT
+{
+    class DataBaseProcess
+    {
+        //Khai báo biến toàn cục, bạn phải thay đổi chuối kết nối phù hợp
+        string strConnect = "Data Source=NGUYENDUC;Initial Catalog=DienThoai;Integrated Security=True";
+        SqlConnection sqlConnect = null;
+        //Phương thức mở kết nối
+        void OpenConnect()
+        {
+            sqlConnect = new SqlConnection(strConnect);
+            if (sqlConnect.State != ConnectionState.Open)
+                sqlConnect.Open();
+        }
+        //Phương thức đóng kết nối
+        void CloseConnect()
+        {
+            if (sqlConnect.State != ConnectionState.Closed)
+            {
+                sqlConnect.Close();
+                sqlConnect.Dispose();
+            }
+        }
+        //Phương thức thực thi câu lệnh Select trả về một DataTable
+        public DataTable DataReader(string sqlSelct)
+        {
+            DataTable tblData = new DataTable();
+            OpenConnect();
+            SqlDataAdapter sqlData = new SqlDataAdapter(sqlSelct, sqlConnect);
+            sqlData.Fill(tblData);
+            CloseConnect();
+            return tblData;
+        }
+        //Phương thức thực hiện câu lệnh dạng insert,update,delete
+        public void DataChange(string sql)
+        {
+            OpenConnect();
+            SqlCommand sqlcomma = new SqlCommand();
+            sqlcomma.Connection = sqlConnect;
+            sqlcomma.CommandText = sql;
+            sqlcomma.ExecuteNonQuery();
+            CloseConnect();
+        }
+
+    }
+}
